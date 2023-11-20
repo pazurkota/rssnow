1abd06edcefd7563d53b3123a6d5a310f0de39fd
Piotr Jelonek <piotrek.jelonek@icloud.com>
2023-11-20 14:04:34 +0100

    Added "RssLink" class

---
+++ rssnow/Model/RssLink.cs
@@ -0,0 +1,12 @@
+﻿using SQLite;
+
+namespace rssnow.Model;
+
+public class RssLink
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+    
+    [Unique]
+    public string Link { get; set; }
+}
