a4f7815d8ac168291db98fa983ca582526c7b476
Piotr Jelonek <piotrek.jelonek@icloud.com>
2023-12-04 15:24:54 +0100

    Added commands binding into RssChannelsPage

--- rssnow/ViewModel/RssChannelsViewModel.cs
+++ rssnow/ViewModel/RssChannelsViewModel.cs
@@ -12,4 +12,41 @@ public partial class RssChannelsViewModel : ObservableObject
     {
         Links = await App.Repository.GetAllLinks();
     }
+
+    [RelayCommand]
+    void ShowAllChannels()
+    {
+        GetAllChannels();
+    }
+
+    [RelayCommand]
+    async Task AddChannel()
+    {
+        string link = await Shell.Current.DisplayPromptAsync("Add RSS Link", "Put RSS Channel link below:");
+
+        if (link == null)
+            return;
+        
+        App.Repository.AddNewLink(link);
+    }
+    
+    [RelayCommand]
+    async Task UpdateChannel()
+    {
+        int id = Int32.Parse(await Shell.Current.DisplayPromptAsync("Edit RSS Channel", "Insert ID of the channel"));
+        string newLink = await Shell.Current.DisplayPromptAsync("Edit RSS Channel", "Insert a new link:");
+
+        List<RssLink> links = await App.Repository.GetAllLinks();
+        string link = links.Find(x => x.Id == id).Link;
+
+        App.Repository.EditLink(link, newLink);
+    }
+
+    [RelayCommand]
+    async Task DeleteChannel()
+    {
+        int id = int.Parse(await Shell.Current.DisplayPromptAsync("Delete RSS Channel", "Insert ID of the channel"));
+        
+        App.Repository.DeleteLink(id);
+    }
 }
--- rssnow/Views/RssChannelsPage.xaml.cs
+++ rssnow/Views/RssChannelsPage.xaml.cs
@@ -10,41 +10,5 @@ public partial class RssChannelsPage : ContentPage
         InitializeComponent();
         BindingContext = new RssChannelsViewModel();
     }
-
-    async void AddRssLinkClicked(object sender, EventArgs e)
-    {
-        string link = await DisplayPromptAsync("Add RSS Link", "Put RSS Channel link below:");
-
-        if (link == null)
-            return;
-        
-        App.Repository.AddNewLink(link);
-        await DisplayAlert("Status", $"{App.Repository.StatusMessage}", "OK");
-    }
-
-    void ShowAllChannelsClicked(object sender, EventArgs e)
-    {
-        var viewModel = (RssChannelsViewModel)BindingContext;
-        
-        viewModel.GetAllChannels();
-    }
-
-    async void UpdateChannelClicked(object sender, EventArgs e)
-    {
-        int id = Int32.Parse(await DisplayPromptAsync("Edit RSS Channel", "Insert ID of the channel"));
-        string newLink = await DisplayPromptAsync("Edit RSS Channel", "Insert a new link:");
-
-        List<RssLink> links = await App.Repository.GetAllLinks();
-        string link = links.Find(x => x.Id == id).Link;
-
-        App.Repository.EditLink(link, newLink);
-    }
-
-    async void DeleteChannelClicked(object sender, EventArgs e)
-    {
-        int id = int.Parse(await DisplayPromptAsync("Delete RSS Channel", "Insert ID of the channel"));
-        
-        App.Repository.DeleteLink(id);
-    }
 }
 
