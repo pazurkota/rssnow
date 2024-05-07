8af5d7d22b063392aef840fc634d29865c4a4418
Piotr Jelonek <piotrek.jelonek@icloud.com>
2024-05-07 19:45:26 +0200

    Created "RSSArticle" class

---
+++ rssnow/RSS_Parser/RSSArticle.cs
@@ -0,0 +1,25 @@
+﻿using System.Xml;
+
+namespace rssnow.RSS_Parser;
+
+public class RSSArticle
+{
+    private readonly string articleTitle;
+    private readonly string articleLink;
+    private readonly string articleDesc;
+    private readonly Dictionary<string, XmlNode> articleVariables;
+
+    public string ArticleTitle => articleTitle;
+    public string ArticleLink => articleLink;
+    public string ArticleDesc => articleDesc;
+    public Dictionary<string, XmlNode> ArticleVariables => articleVariables;
+
+    public RSSArticle(string ArticleTitle, string ArticleLink, string ArticleDesc,
+        Dictionary<string, XmlNode> ArticleVariables)
+    {
+        articleTitle = !string.IsNullOrWhiteSpace(ArticleTitle) ? ArticleTitle.Trim() : "";
+        articleLink = !string.IsNullOrWhiteSpace(ArticleLink) ? ArticleLink.Trim() : "";
+        articleDesc = !string.IsNullOrWhiteSpace(ArticleDesc) ? ArticleDesc.Trim() : "";
+        articleVariables = ArticleVariables;
+    }
+}
